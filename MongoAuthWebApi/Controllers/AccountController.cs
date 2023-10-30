48f28c8f44995008863aa1dadbe930116f3cea38
Sherwin Gonzales <Sherwin.Gonzales@onehema.com>
2023-10-30 11:16:11 +0800

    Refactored AccountController

--- MongoAuthWebApi/Controllers/AccountController.cs
+++ MongoAuthWebApi/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MongoAuthWebApi.Commands;
+using MongoAuthWebApi.DTOs;
 using MongoAuthWebApi.Models;
 
 namespace MongoAuthWebApi.Controllers;
@@ -15,6 +16,8 @@ public class AccountController : ControllerBase
 
     [HttpPost("login")]
     [AllowAnonymous]
+    [ProducesResponseType(typeof(LoginDTO), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Login(LoginModel loginModel)
     {
         var loginCommand = new LoginCommand(loginModel.Email, loginModel.Password);
@@ -26,13 +29,15 @@ public class AccountController : ControllerBase
 
     [HttpPost("register")]
     [AllowAnonymous]
+    [ProducesResponseType(typeof(Guid), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Register(RegisterModel registerModel)
     {
         var registerCommand = new RegisterCommand(registerModel.Email, registerModel.Password);
 
         var result = await Mediator.Send(registerCommand);
 
-        if (result.IsSuccess) return Ok(result.Value);
+        if (result.IsSuccess) return CreatedAtAction(nameof(this.Register), result.Value);
         return BadRequest(result.Error);
     }
 }
